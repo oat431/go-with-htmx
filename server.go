44f68d95d31d9cd15772217cedada5311837a9e0
oat431 <oat431@gmail.com>
2025-10-29 23:41:23 +0700

    add: viper package foe .env

--- server.go
+++ server.go
@@ -4,13 +4,33 @@ import (
 	"go_htmx/service"
 	"log"
 	"net/http"
+
+	"github.com/spf13/viper"
 )
 
+func viperEnvVariable(key string) string {
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatal("Error while reading .env")
+	}
+
+	value, ok := viper.Get(key).(string)
+
+	if !ok {
+		log.Fatal("Invalid type")
+	}
+
+	return value
+}
+
 func main() {
+	port := viperEnvVariable("PORT")
+
 	http.HandleFunc("/", service.RootHandler)
 	http.HandleFunc("/increment", service.IncrementHandler)
 
-	port := "8080"
 	log.Printf("Go HTMX Server starting on http://localhost:%s", port)
 
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
